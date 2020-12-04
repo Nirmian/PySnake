a5d04b24193780b8cc5e1b420f9dc5620c6d629a
Dorin <mirondorin99@gmail.com>
2020-12-05 00:32:50 +0200

    Added score ui. Solved size 2 snake issue

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ CELL_COLS = 20
 HEIGHT = CELL_SIZE * CELL_COLS
 WIDTH = CELL_SIZE * CELL_ROWS
 FPS = 60
+VALID_KEYS = [pygame.K_RIGHT, pygame.K_LEFT, pygame.K_UP, pygame.K_DOWN]
 
 pygame.init()
 screen = pygame.display.set_mode([HEIGHT, WIDTH])
@@ -26,6 +27,7 @@ class Snake:
     def __init__(self, px, py):
         self.body = [Vector2(px - 1, py), Vector2(px,py)]
         self.dir = Vector2(0, 0)
+        self.last_dir = Vector2(-100, -100)
 
     def draw_snake(self):
         for part in self.body:
@@ -46,9 +48,25 @@ class Snake:
     def eat(self):
         self.body.insert(0, self.body[0] + self.dir)
 
+class Obstacle:
+    def __init__(self):
+        self.obstacles = []
+
+class GameUI:
+    def __init__(self):
+        self.font = pygame.font.SysFont(None, 36)
+        self.score = 0
+        self.score_text = self.font.render('Score: ' + str(self.score), True, (0, 255, 0))
+    def update_score(self):
+        self.score += 1
+        self.score_text = self.font.render('Score: ' + str(self.score), True, (0, 255, 0))
+    def draw_ui(self):
+        screen.blit(self.score_text, self.score_text.get_rect())
+
 if __name__ == "__main__":
     UPDATE_STATE = pygame.USEREVENT
     pygame.time.set_timer(UPDATE_STATE, 100)
+    ui = GameUI()
     snake = Snake(2, 2)
     food = Food()
 
@@ -60,14 +78,16 @@ if __name__ == "__main__":
             if event.type == UPDATE_STATE:
                 snake.move()
                 snake.warp()
-                if snake.body[0][0] == food.food_px and snake.body[0][1] == food.food_py:
-                    snake.eat()
-                    food = Food()
-                #TODO when snake body is size 2 snake can't eat itself.
-                elif snake.body[0] in snake.body[1:] and snake.dir != Vector2(0,0):
+                if (snake.body[0] in snake.body[1:] and snake.dir != Vector2(0,0)) or snake.last_dir * (-1) == snake.dir:
                     print("You lost")
                     pygame.quit()
-            if event.type == pygame.KEYDOWN:
+                elif snake.body[0][0] == food.food_px and snake.body[0][1] == food.food_py:
+                    snake.eat()
+                    food = Food()
+                    ui.update_score()
+            if event.type == pygame.KEYDOWN and event.key in VALID_KEYS:
+                snake.last_dir = snake.dir
+
                 if event.key == pygame.K_RIGHT:
                     snake.dir = Vector2(1, 0)
                 elif event.key == pygame.K_DOWN:
@@ -78,6 +98,7 @@ if __name__ == "__main__":
                     snake.dir = Vector2(0, -1)
 
         screen.fill((0,0,0))
+        ui.draw_ui()
         snake.draw_snake()
         food.draw_food()
         pygame.display.update()
